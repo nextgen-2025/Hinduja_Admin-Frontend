14121181be528276e42c0e0ce0f52020a24a5bcd
Nextgen25 <nextgenworks93@gmail.com>
2025-04-16 10:17:13 +0530

    update the logo

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,7 +6,7 @@ import { AdminContext } from '../context/AdminContext'
 
 const Sidebar = () => {
 
-  const { dToken } = useContext(DoctorContext)
+  const { dToken } = useContext(DoctorContext)   
   const { aToken } = useContext(AdminContext)
 
   return (
@@ -30,7 +30,7 @@ const Sidebar = () => {
           <p className='hidden md:block'>Doctors List</p>
         </NavLink>
       </ul>}
-
+   
       {dToken && <ul className='text-[#515151] mt-5'>
         <NavLink to={'/doctor-dashboard'} className={({ isActive }) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-primary' : ''}`}>
           <img className='min-w-5' src={assets.home_icon} alt='' />
